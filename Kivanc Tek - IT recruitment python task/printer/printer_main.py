04676a85aeb1cf4f2052545c1326a5442832b4f3
padron63 <96939504+padron63@users.noreply.github.com>
2025-04-06 23:16:44 +0300

    Update printer_main.py

--- Kivanc Tek - IT recruitment python task/printer/printer_main.py
+++ Kivanc Tek - IT recruitment python task/printer/printer_main.py
@@ -1,15 +1,6 @@
-import os
-from fpp import Peripheral, Notifier, Listener
-from Listener import Poli_Listener
+PIPE_PATH = "/tmp/Policumbent_pipe"
+pipe = open(PIPE_PATH, "r")
 
-PIPE_PATH = "Policumbent_pipe"
-DEVICE_PATH = "peripheral_device"
-
-if not os.path.exists(PIPE_PATH):
-    os.mkfifo(PIPE_PATH)
-
-
-
-peripheral = Peripheral(DEVICE_PATH)
-listener = Poli_Listener(PIPE_PATH)
-notifier = Notifier(peripheral, listener)
+for message in pipe:
+    if message:
+        print(message.strip())
